a59f3596e49eb914834da1a6dda57bd3dd13eb2b
raaf882 <rahaf@example.com>
2026-07-09 08:53:37 +0300

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,11 @@
 const path = require("path");
+const fs = require("fs");
 const Database = require("better-sqlite3");
 
 const dbPath = path.join(__dirname, "..", "database", "data.sqlite");
+fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 const db = new Database(dbPath);
+
 
 db.pragma("journal_mode = WAL");
 
